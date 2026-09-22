fea6f092d25e98fc0424670d1348f9d4222e81d7
drealslick <olumuyiwaemmanuel47@gmail.com>
2026-09-22 03:36:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ConditionDetail from './pages/ConditionDetail';
 import FirstVisit from './pages/FirstVisit';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Pricing from './pages/Pricing';
 
 function AppShell() {
   const [, setMobileMenuOpen] = useState(false);
@@ -45,6 +46,7 @@ function AppShell() {
             <Route path="/first-visit" element={<FirstVisit />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/pricing" element={<Pricing />} />
           </Routes>
         </main>
 
